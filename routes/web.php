15f89ba3ec1692d55261a9d4770c9ab072eb2754
Ethan Chang <zyyhaw@gmail.com>
2022-06-02 18:51:02 +0800

    Restart password rewrite trait logic

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::post('register', 'Auth\RegisterController@register');
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 // 再次确认密码（重要操作前提示）
 Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
