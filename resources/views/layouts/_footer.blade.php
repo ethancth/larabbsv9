7622291d7b90f313c0b3d7b6562f7eb2ee13755d
Ethan Chang <zyyhaw@gmail.com>
2022-06-17 00:51:06 +0800

    page setting

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,8 +2,7 @@
   <div class="container">
     <p class="float-start">
       由 <a href="" target="_blank">Ethan</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
-    </p>
-
-    <p class="float-end"><a href="mailto:name@email.com">联系我们</a></p>RouteServiceProvider
+    </p>\
+    <p class="float-end"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
